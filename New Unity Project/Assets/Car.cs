43e95a3b99306e743771157c1cfcfd735aaaed71
陳宛昀 <lov011052@gmail.com>
2020-04-08 15:29:45 +0800

    認識欄位

--- New Unity Project/Assets/Car.cs
+++ New Unity Project/Assets/Car.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 // 註解：用來說明紀錄等等.. 只會在此腳本內看到
-// 摘要summary：描述此內容，會在其他腳本內看到-提高維護性與擴充性
+// 摘要summary：描述此內容，會在其他腳本內看到-提高維護性與擴充性、可讀性
+// 新增的C#放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
 ///  定義汽車物件
 /// </summary>
@@ -16,13 +18,36 @@ public class Car : MonoBehaviour
     // 煞車:是否開啟-使用布林值 bool
 
     // 定義欄位語法:
-    // 資料類型 欄位名稱 (指定 值 可省略);
+    // 修飾詞 資料類型 欄位名稱 (指定 值 可省略);
+    // 公開 public : 允許其他類別存取，顯示在屬性面板(Inspector)上
+    // 私人 private (預設) : 不允許、不顯示
     // 預設值 int float為0
     // string為""
     // bool 為 false
+    // 欄位屬性Attribute：Unity 提供用於輔助欄位的功能
+    // 語法：[屬性名稱(值)]
+    // 標題[Header("字串")]
+    // 提示[Tooltip("字串")]
+    // 範圍[Range(最小值,最大值)] -只能使用在數值類型的資料 ex: int、float
 
-    int ccCount = 2000; // 對應第13行
-    float weight = 100.9f; // 對應第14行
-    string brand = "BMW"; // 對應第15行
-    bool brake = false; // 對應第16行
+    /// <summary>
+    /// 汽車的cc數
+    /// </summary>
+    [Header("汽車的cc數"), Tooltip("這個欄位是用來存放汽車的cc數"), Range(500,5000)]
+    public int ccCount = 2000; // 對應第13行
+    /// <summary>
+    /// 汽車的重量
+    /// </summary>
+    [Header("汽車的重量"),Tooltip("這個欄位是用來存放汽車的噸位"),Range(50.3f,500)]
+    public float weight = 100.9f; // 對應第14行
+    /// <summary>
+    /// 汽車的品牌
+    /// </summary>
+    [Header("汽車的品牌"),Tooltip("代表這台車子的品牌")]
+    public string brand = "BMW"; // 對應第15行
+    /// <summary>
+    /// 汽車的煞車是否開啟
+    /// </summary>
+    [Header("汽車的煞車是否開啟"),Tooltip("用來儲存這台車子目前是否為煞車狀態")]
+    public bool brake = false; // 對應第16行
 }
--- New Unity Project/Assets/Class1.cs
+++ New Unity Project/Assets/Class1.cs
@@ -11,6 +11,14 @@ public class Class1 : MonoBehaviour
     // 1. 大小不一樣
     // 2. 括號成對出現
 
+    [Header("汽車1號")]
+    public Car car1;
+    // 存放帶有Car的物件
+
+    [Header("汽車2號")]
+    public Car car2;
+
+
     // 事件:在特定時間會已指定次數執行的方法，名稱是藍色
     // 開始事件:播放遊戲時執行一次-初始設定
     private void Start()
@@ -18,5 +26,16 @@ public class Class1 : MonoBehaviour
         // 輸出("文字訊息");
 
         print("哈囉．沃德");
+
+        // 取得 get
+        print("汽車1號的CC數:"+car1.ccCount+",汽車1號的重量:"+car1.weight);
+        print("汽車2號的品牌:"+car2.brand+",汽車2號的煞車是否啟用:"+car2.brake);
+
+        // 設定 set
+        car1.brake = true;
+        car1.brand = "TOYOTA";
+        car2.brake = true;
+        car2.weight = 99.5f;
+
     }
 }
